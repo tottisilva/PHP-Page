e86b9cafdf66a90f54f36ee52384015061bea6f6
tottisilva <73293985+tottisilva@users.noreply.github.com>
2020-11-05 14:19:18 +0000

    first commit

---
+++ _modelophp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="_css/estilo.css" />
+  <meta charset="UTF-8" />
+  <title> PHP </title>
+</head>
+
+<body>
+  <div>
+    <?php
+
+    ?>
+  </div>
+</body>
+
+</html>
